59953543151bd70e69f4c54d2baeb01fc65a2d64
Lowella369 <lh850210@gmail.com>
2022-01-18 10:05:30 -0600

    Used axios instead of fetch

--- src/api.js
+++ src/api.js
@@ -1,9 +1,10 @@
 import axios from "axios"
 export const fetchData = async () => {
   try {
-    const response = await fetch("https://jsonplaceholder.typicode.com/todos/1");
-    const data = await response.json();
-    return data;
+    return axios ({
+      method: 'get',
+      url: 'https://jsonplaceholder.typicode.com/todos/1'
+  }).then( resp => resp.data );
   } catch (e) {
     console.log(e);
   }
